873c02fe7d24677f9232ce8866878697f96fdcb6
Matheus Muriel <matheus.muriel@outlook.com>
2021-01-27 13:01:54 -0300

    Modularizado

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,43 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
-/** function Title(props){
-  return (
-    <h1>
-      {props.children}
-    </h1>
-  )
-} */
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg} >
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            Titulo
+          </Widget.Header>
+          <Widget.Content>
+            Pergunta
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            Pergunta 2 
+          </Widget.Content>
+        </Widget>
+        <Footer></Footer>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/MatheusMuriel/ImersaoReact-NextJS" />
+    </QuizBackground>
+  )
 }

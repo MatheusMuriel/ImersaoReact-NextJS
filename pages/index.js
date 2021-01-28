4e496b3207a8d96b7994d1c1440f6a4ee3d88344
Matheus Muriel <matheus.muriel@outlook.com>
2021-01-28 11:21:47 -0300

    QuizContainer component

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,7 @@ import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
   const router = useRouter();
@@ -56,7 +46,8 @@ export default function Home() {
 
         <Widget>
           <Widget.Content>
-            Pergunta 2
+            <h1>Quizes da Galera</h1>
+            <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
         </Widget>
         <Footer />
